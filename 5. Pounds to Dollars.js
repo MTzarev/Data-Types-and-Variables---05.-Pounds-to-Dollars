639a6688ff8c102b58ac9412265e1722c24911ac
Miroslav Tzarev <mtzarev@gmail.com>
2024-01-19 18:28:32 +0200

    Add files via upload

---
+++ 5. Pounds to Dollars.js
@@ -0,0 +1,10 @@
+function poundsToDollars(pounds) {
+    let dollars = (pounds * 1.31).toFixed(3);
+
+
+
+    //1 British Pound = 1.31 Dollars
+    console.log(dollars);
+}
+//poundsToDollars(80);
+poundsToDollars(39);
